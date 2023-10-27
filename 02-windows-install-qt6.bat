d13f562e94b662c1de95bb4c4d4d8fbea9b23519
Gilles Caulier <caulier.gilles@gmail.com>
2023-10-27 15:51:06 +0200

    add more deps

--- 02-windows-install-qt6.bat
+++ 02-windows-install-qt6.bat
@@ -17,17 +17,11 @@ call config.cmd
 REM For elapsed time mesurement
 call %ORIG_WD%/common.cmd :StartScript
 
+%VCPKG_DIR%/vcpkg %VCPKG_COMMON_OPTIONS% install libmariadb
 %VCPKG_DIR%/vcpkg %VCPKG_COMMON_OPTIONS% install qt
 
-REM %VCPKG_DIR%/vcpkg %VCPKG_COMMON_OPTIONS% install qtbase
-REM %VCPKG_DIR%/vcpkg %VCPKG_COMMON_OPTIONS% install qtimageformats
-REM %VCPKG_DIR%/vcpkg %VCPKG_COMMON_OPTIONS% install qtmultimedia
-REM %VCPKG_DIR%/vcpkg %VCPKG_COMMON_OPTIONS% install qtnetworkauth
-REM #%VCPKG_DIR%/vcpkg %VCPKG_COMMON_OPTIONS% install qtscxml
-REM #%VCPKG_DIR%/vcpkg %VCPKG_COMMON_OPTIONS% install qtsvg
-REM %VCPKG_DIR%/vcpkg %VCPKG_COMMON_OPTIONS% install qttranslations
-REM %VCPKG_DIR%/vcpkg %VCPKG_COMMON_OPTIONS% install qttools
-REM #%VCPKG_DIR%/vcpkg %VCPKG_COMMON_OPTIONS% install qtwebengine
+%VCPKG_DIR%/vcpkg %VCPKG_COMMON_OPTIONS% install libheif
+%VCPKG_DIR%/vcpkg %VCPKG_COMMON_OPTIONS% install libexiv2
 
 REM For elapsed time mesurement
 
