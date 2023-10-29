dcae8f38be86d7aadb9acfa26fa61012a1b7190d
Gilles Caulier <caulier.gilles@gmail.com>
2023-10-29 11:48:13 +0100

    port++

--- 01-build-vcpkg.sh
+++ 01-build-vcpkg.sh
@@ -87,38 +87,38 @@ git pull
 #################################################################################################
 # Dependencies build and installation
 
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install openssl
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install pthreads
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install gettext
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install icu
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install libpng
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install tiff
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install libmysql
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install libjpeg-turbo
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install jasper
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install libde265
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install libjxl
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install aom
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install avif
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install libheif
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install freeglut
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install brotli
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install zlib
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install bzip2
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install ffmpeg
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install qt
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install openssl
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install pthreads
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install gettext
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install icu
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install libpng
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install tiff
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install libmysql
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install libjpeg-turbo
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install jasper
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install libde265
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install libjxl
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install aom
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install avif
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install libheif
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install freeglut
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install brotli
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install zlib
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install bzip2
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install ffmpeg
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install qt
 
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install opencv
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install boost
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install lcms
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install eigen3
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install expat
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install libxml2
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install libxslt
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install exiv2
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install libical
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install fftw3
-$VCPKG_DIR/vcpkg $VCPKG_COMMON_OPTIONS install openexr
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install opencv
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install boost
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install lcms
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install eigen3
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install expat
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install libxml2
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install libxslt
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install exiv2
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install libical
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install fftw3
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install openexr
 
 echo -e "\n"
 
